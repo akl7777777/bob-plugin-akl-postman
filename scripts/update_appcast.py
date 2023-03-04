6bb76e140d8d247be80f990562abbba4121749e5
akl7777777 <akl7777777@163.com>
2023-03-04 23:55:16 +0800

    测自动化版本号

--- scripts/update_appcast.py
+++ scripts/update_appcast.py
@@ -8,7 +8,7 @@ def update_appcast(message):
     with open ("src/info.json", "r") as f:
         info = json.load(f)
     version = info["version"]
-    release_file = Path(f"release/bob-plugin-akl-postman_v{version}.bobplugin")
+    release_file = Path(f"release/bob-plugin-akl-postman.bobplugin")
     assert release_file.is_file(), "Release file not exist"
     with open(release_file, "rb") as f:
         c = f.read()
