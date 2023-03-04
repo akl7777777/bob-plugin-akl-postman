c84c5a466926ad500b121e0f2a4eb407fe2bcff7
akl7777777 <akl7777777@163.com>
2023-03-04 23:47:51 +0800

    修改自动化脚本,让产出物带上版本号

--- scripts/update_appcast.py
+++ scripts/update_appcast.py
@@ -8,7 +8,7 @@ def update_appcast(message):
     with open ("src/info.json", "r") as f:
         info = json.load(f)
     version = info["version"]
-    release_file = Path(f"release/bob-plugin-akl-postman.bobplugin")
+    release_file = Path(f"release/bob-plugin-akl-postman_v{version}.bobplugin")
     assert release_file.is_file(), "Release file not exist"
     with open(release_file, "rb") as f:
         c = f.read()
@@ -17,7 +17,7 @@ def update_appcast(message):
         "version": version,
         "desc": message,
         "sha256": file_hash,
-        "url": f"https://github.com/akl7777777/bob-plugin-akl-postman/releases/download/v{version}/bob-plugin-akl-postman.bobplugin",
+        "url": f"https://github.com/akl7777777/bob-plugin-akl-postman/releases/download/v{version}/bob-plugin-akl-postman_v{version}.bobplugin",
         "minBobVersion": "0.5.0"
     }
     appcast_file = Path("appcast.json")
